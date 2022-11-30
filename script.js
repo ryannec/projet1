6811f69275173db2da5f33044e0ee6951e45154d
ryannec <anthonydieuad@gmail.com>
2022-11-30 16:37:53 +0100

    responsive header

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ let buttonBurger = document.querySelector(".menu-burger");
 let invisible = document.querySelector(".invisible")
 buttonBurger.addEventListener('click',() => {
 invisible.classList.toggle("visible");
-})
+});
+let grandLogo = document.querySelector(".grandlogo")
+let body = document.querySelector("body");
+
+if(body.width > "600px"){
+grandLogo.src = "/assets/image d'accueil.jpg";
+body.style.backgroundColor = "blue";
+}
