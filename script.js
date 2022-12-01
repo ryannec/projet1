13b4835aee3d9d83da55a4df56eab29f60f64d2e
ryannec <anthonydieuad@gmail.com>
2022-12-01 19:19:27 +0100

    bug background menu burger

--- script.js
+++ script.js
@@ -3,10 +3,3 @@ let invisible = document.querySelector(".invisible")
 buttonBurger.addEventListener('click',() => {
 invisible.classList.toggle("visible");
 });
-let grandLogo = document.querySelector(".grandlogo")
-let body = document.querySelector("body");
-
-if(body.width > "600px"){
-grandLogo.src = "image d'accueil.jpg";
-body.style.backgroundColor = "blue";
-}
