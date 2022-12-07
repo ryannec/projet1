99aa58b8db42cb8c4c4bbca768f33b1004fb4ce8
DIEU Anthony (SNCF VOYAGEURS / TECHNICENTRE DE VENISSIEUX / Equ Maintenance) <8402956y@commun.ad.sncf.fr>
2022-12-07 18:33:41 +0100

    ajout de l'animation du menu B et font F

--- script.js
+++ script.js
@@ -3,10 +3,3 @@ let invisible = document.querySelector(".invisible");
 buttonBurger.addEventListener("click", () => {
   invisible.classList.toggle("visible");
 });
-
-// let imageStadeMini = document.querySelector(".stade-mini");
-// let imageStadeMaxi = document.querySelector(".stade-maxi");
-
-// imageStadeMini.addEventListener('click',() => {
-//   imageStadeMaxi.classList.toggle ("stade-maxi");
-// });
