c82b6d8827fa9e1998cca8426775f76f75bd00e1
DIEU Anthony (SNCF VOYAGEURS / TECHNICENTRE DE VENISSIEUX / Equ Maintenance) <8402956y@commun.ad.sncf.fr>
2022-12-07 13:26:51 +0100

    fin sans animation

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let buttonBurger = document.querySelector(".menu-burger");
-let invisible = document.querySelector(".invisible")
-buttonBurger.addEventListener('click',() => {
-invisible.classList.toggle("visible");
+let invisible = document.querySelector(".invisible");
+buttonBurger.addEventListener("click", () => {
+  invisible.classList.toggle("visible");
 });
+
+// let imageStadeMini = document.querySelector(".stade-mini");
+// let imageStadeMaxi = document.querySelector(".stade-maxi");
+
+// imageStadeMini.addEventListener('click',() => {
+//   imageStadeMaxi.classList.toggle ("stade-maxi");
+// });
